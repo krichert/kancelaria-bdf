aba66a68d6458e2959f904d440dc1b28d7901f3f
Kamil Richert <krichert@atlassian.com>
2023-09-24 23:44:11 +0200

    :)

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 (function () {
-    var arrow = document.getElementById("information-link");
-    arrow.addEventListener("click", clickHandler);
+    var observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const p = entry.target;
 
-    function clickHandler(e) {
-        e.preventDefault();
-        const href = this.getAttribute("href");
-        const offsetTop = document.querySelector(href).offsetTop;
+            if (entry.isIntersecting) {
+                p.classList.add('animatetop');
+                return;
+            }
 
-        scroll({
-            top: offsetTop,
-            behavior: "smooth"
+            p.classList.remove('animatetop');
         });
-    }
+    });
+
+    observer.observe(document.querySelector('#about p'));
+
+    var observer2 = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const p = entry.target;
+
+            if (entry.isIntersecting) {
+                p.classList.add('animatetop');
+                return;
+            }
+
+            p.classList.remove('animatetop');
+        });
+    });
+
+    observer2.observe(document.querySelector('#about p.py-2'));
+
+    var cards = document.querySelectorAll('.flip-card')
+    cards.forEach(card => {
+
+        card.addEventListener('mouseenter', function () {
+            card.classList.add('flip-card-active')
+        })
+
+        card.addEventListener('mouseleave', function () {
+            card.classList.remove('flip-card-active')
+        })
+    })
 })();
